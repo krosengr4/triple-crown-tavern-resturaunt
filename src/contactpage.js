beeb326f973cf6f7e150ed87dbfbf4a85cfdbbb2
Kevin Rosengren <rosenkev4@gmail.com>
2026-08-05 17:55:18 -0500

    Added logic to create a title on the contact page. Set up contact page load function with index script to load when contact nav button is clicked

---
+++ src/contactpage.js
@@ -0,0 +1,49 @@
+const CONTACTS = [
+    {
+        name: "Babe Ruth",
+        title: "Founder",
+        phone: "465-003-2727",
+        email: "greatOne03@mail.com",
+    },
+    {
+        name: "Derek Jeter",
+        title: "Captain",
+        phone: "222-222-2227",
+        email: "elCaptain@realmail.com",
+    },
+    {
+        name: "Joe Torre",
+        title: "Manager",
+        phone: "123-456-7890",
+        email: "joe@yankees.com",
+    },
+    {
+        name: "Mickey Mantle",
+        title: "HOF Legend",
+        phone: "777-777-0707",
+        email: "theMick@mickey.com",
+    },
+];
+
+export function loadContactPage() {
+    const page = document.createElement("div");
+    page.id = "content";
+
+    const title = createTitle();
+
+    page.appendChild(title);
+
+    return page;
+}
+
+function createTitle() {
+    const titleDiv = document.createElement("div");
+    titleDiv.id = "pageTitle";
+
+    const titleHeader = document.createElement("h1");
+    titleHeader.id = "title";
+    titleHeader.textContent = "Contact Us";
+
+    titleDiv.appendChild(titleHeader);
+    return titleDiv;
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./style.css";
 import { loadHomepage } from "./homepage.js";
 import { loadMenupage } from "./menupage.js";
+import { loadContactPage } from "./contactpage.js";
 
 const contentDiv = document.querySelector("#content");
 
 const homeBtn = document.querySelector("#homeButton");
 const menuBtn = document.querySelector("#menuButton");
+const contactBtn = document.querySelector("#contactButton");
 
 function loadHome() {
     contentDiv.replaceChildren(loadHomepage());
@@ -15,6 +17,11 @@ function loadMenu() {
     contentDiv.replaceChildren(loadMenupage());
 }
 
+function loadContact() {
+    contentDiv.replaceChildren(loadContactPage());
+}
+
 homeBtn.addEventListener("click", loadHome);
 menuBtn.addEventListener("click", loadMenu);
+contactBtn.addEventListener("click", loadContact);
 // loadHome();
