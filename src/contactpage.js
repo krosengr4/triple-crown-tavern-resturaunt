9127693a2fed944bf5c78710d6817354afeb28b3
Kevin Rosengren <rosenkev4@gmail.com>
2026-08-06 02:59:15 -0500

    Added logic to load the contact us page with js

--- src/contactpage.js
+++ src/contactpage.js
@@ -30,8 +30,10 @@ export function loadContactPage() {
     page.id = "content";
 
     const title = createTitle();
+    const content = createContactCards();
 
     page.appendChild(title);
+    page.appendChild(content);
 
     return page;
 }
@@ -46,4 +48,52 @@ function createTitle() {
 
     titleDiv.appendChild(titleHeader);
     return titleDiv;
+}
+
+function createContactCards() {
+    const cardsDiv = document.createElement("div");
+    cardsDiv.id = "nameContent";
+
+    const leftCards = document.createElement("div");
+    leftCards.id = "leftNames";
+    const rightCards = document.createElement("div");
+    rightCards.id = "rightNames";
+
+    for (let i = 0; i < CONTACTS.length; i++) {
+        const contactCard = document.createElement("div");
+        contactCard.id = "nameCard";
+
+        const name = document.createElement("h2");
+        name.id = "name";
+        name.textContent = `${CONTACTS[i].name}`;
+
+        const title = document.createElement("p");
+        title.id = "info";
+        title.classList = "contactTitle";
+        title.textContent = `${CONTACTS[i].title}`;
+
+        const phone = document.createElement("p");
+        phone.id = "info";
+        phone.textContent = `${CONTACTS[i].phone}`;
+
+        const email = document.createElement("p");
+        email.id = "info";
+        email.textContent = `${CONTACTS[i].email}`;
+
+        //adgfsdafg
+        contactCard.appendChild(name);
+        contactCard.appendChild(title);
+        contactCard.appendChild(phone);
+        contactCard.appendChild(email);
+
+        if (i % 2 === 0) {
+            leftCards.appendChild(contactCard);
+        } else {
+            rightCards.appendChild(contactCard);
+        }
+    }
+
+    cardsDiv.appendChild(leftCards);
+    cardsDiv.appendChild(rightCards);
+    return cardsDiv;
 }
