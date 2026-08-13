93843e186c4bc93ffaa8c9a9c99484611dfb7849
Kevin Rosengren <rosenkev4@gmail.com>
2026-08-12 22:33:05 -0500

    Moved contact data into data directory

--- src/contactpage.js
+++ src/contactpage.js
@@ -1,29 +1,4 @@
-const CONTACTS = [
-    {
-        name: "Babe Ruth",
-        title: "Founder",
-        phone: "465-003-2727",
-        email: "greatOne03@mail.com",
-    },
-    {
-        name: "Derek Jeter",
-        title: "Captain",
-        phone: "222-222-2227",
-        email: "elCaptain@realmail.com",
-    },
-    {
-        name: "Joe Torre",
-        title: "Manager",
-        phone: "123-456-7890",
-        email: "joe@yankees.com",
-    },
-    {
-        name: "Mickey Mantle",
-        title: "HOF Legend",
-        phone: "777-777-0707",
-        email: "theMick@mickey.com",
-    },
-];
+import { CONTACTS } from "./data/contacts.js";
 
 export function loadContactPage() {
     const page = document.createElement("div");
---
+++ src/data/contacts.js
@@ -0,0 +1,28 @@
+const CONTACTS = [
+    {
+        name: "Babe Ruth",
+        title: "Founder",
+        phone: "465-003-2727",
+        email: "greatOne03@mail.com",
+    },
+    {
+        name: "Derek Jeter",
+        title: "Captain",
+        phone: "222-222-2227",
+        email: "elCaptain@realmail.com",
+    },
+    {
+        name: "Joe Torre",
+        title: "Manager",
+        phone: "123-456-7890",
+        email: "joe@yankees.com",
+    },
+    {
+        name: "Mickey Mantle",
+        title: "HOF Legend",
+        phone: "777-777-0707",
+        email: "theMick@mickey.com",
+    },
+];
+
+export { CONTACTS };
